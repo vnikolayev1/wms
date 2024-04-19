91f6142df458a9b5132585b24846e35909c7c398
vnikolayev1 <vasiliy.nikolayev@weko.com>
2024-04-19 12:43:53 +0300

    [ADD] sale_shopfloor_reception_refund_return
    - Added module for sale order that marks created return as to refund, sets sale order line.

---
+++ sale_shopfloor_reception_refund_return/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 vnikolayev1 Raumschmiede GmbH
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Shopfloor Reception Refund Return",
+    "summary": "Marks created return as to refund, sets sale order line",
+    "version": "14.0.1.0.0",
+    "category": "Inventory, Accounting",
+    "website": "https://github.com/OCA/wms",
+    "author": "Raumschmiede GmbH, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "depends": ["sale_stock", "shopfloor_reception_refund_return"],
+}
---
+++ sale_shopfloor_reception_refund_return/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+Sale Shopfloor Reception Refund Return
+--------------------------------------
+
+Marks returns created by shopfloor as to refund, connects related sale order line.
+
+Sets `to_refund` to true and `sale_line_id` when creating a return move.
